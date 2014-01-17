b27cf722a52898813e1cc92d4f4071bd2c84dfea
Ian Joynes <ian@server.fake>
2014-01-17 10:35:52 -0500

    coanda application
    Documentation for job application to Coanda for the position of Fluid
    Dynamics Research Engineer (Mechanical or Chemical)

---
+++ coanda_fluid_dynamics_research_engineer/e-mail.md
@@ -0,0 +1,15 @@
+from: <ian@joynes.com>
+to: <hr@coanda.ca>
+date: Thu, Jan 16, 2014 at 4:42 PM
+subject: Fluid Dynamics Research Engineer Application (PM2013-08)
+
+To the Hiring Manager for Fluid Dynamics Research Engineer,
+
+I am a Master of Applied Science graduate in mechanical engineering, with a research background in computational fluid dynamics and numerical optimization.  Please accept this as my formal application for the fluid dynamics research engineer position (PM2013-08).  I believe that my skills  could make significant contributions to process flow modelling and design optimization projects within Coanda. Attached are copies of my resume, cover letter and transcript.
+
+Sincerely,
+Ian Joynes
+
+[resume.pdf](https://github.com/ijoynes/job_applications/coanda_fluid_dynamics_research_engineer/resume.pdf)
+[cover_letter.pdf](https://github.com/ijoynes/job_applications/coanda_fluid_dynamics_research_engineer/cover_letter.pdf)
+[transcript.pdf](https://github.com/ijoynes/job_applications/transcript_2014-01-07.pdf)
---
+++ coanda_fluid_dynamics_research_engineer/posting_files/style.php
@@ -0,0 +1,123 @@
+
+#toppanel, #toppanel a,.lowerfooter,#wrapper, .tweets h3 a {
+	color:#5f5f5f;
+}	
+
+.sidebar-content li a, #footer a, #panel a  {
+	color: #D40000;
+}
+
+.sidebar-content li a:hover, #toppanel a:hover{
+	color: #990000;
+
+}
+
+a, div.item-list-tabs#subnav a.show-hide-new, div.item-list-tabs#subnav a.new-reply-link {
+	color: #D40000;
+  	text-decoration: none;
+	outline: none; /* firefox fix  */
+}
+
+a:hover,#sub-tabs ul li.current_page_item,div.item-list-tabs#subnav a.show-hide-new:hover,div.item-list-tabs#subnav a.new-reply-link:hover {
+	color: #990000;
+}
+
+ul.paging li.pagebutton { 
+	background-color: #D40000;
+}
+
+ul.paging li.pagebutton.active, ul.paging a li.pagebutton { 
+	background-color: #990000;
+}
+
+#sub-tabs ul li a {
+	color:#b4b4b4;
+}
+
+acronym, abbr {
+	border-bottom: 1px dashed #D40000;
+}
+
+
+
+
+
+.slidernav-left a {
+	background:#D40000 url(images/gallery-nav.png) top left no-repeat;
+}
+.slidernav-right a {
+	background:#D40000 url(images/gallery-nav.png) top right no-repeat;
+}
+
+.slidernav-left a:hover {
+	background:#990000 url(images/gallery-nav.png) top left no-repeat;
+}
+
+.slidernav-right a:hover {
+	background:#990000 url(images/gallery-nav.png) top right no-repeat;
+}
+
+
+
+
+
+#content .textresize ul li {
+	background-color:#D40000;
+}
+
+#content .textresize ul li:hover {
+	background-color:#990000;
+}
+
+
+#content ul li.socialinit, #content ul li.socialhide:hover, #content .socialicons ul li, #content .postmetadata .comments.yes, .twitterfollow a {
+	background-color:#D40000;
+}
+
+#content ul li.socialinit:hover,#content ul li.socialhide, #content .socialicons ul li:hover, #content .postmetadata .comments.yes:hover, .twitterfollow a:hover  {
+	background-color:#990000;
+}
+
+
+
+#wp-calendar td a {
+	background-color:#D40000;
+}
+
+#wp-calendar td a:hover {
+	background-color:#990000;
+}
+
+.tab a.open {
+background-color:#D40000;
+}
+
+.tab a.close {
+background-color:#D40000;
+}
+
+.tab a:hover.open {
+background-color:#990000;
+}
+
+.tab a:hover.close {
+background-color:#990000;
+}
+
+.panelcontent h2 a, .panelcontent h2 {
+	color:#5f5f5f;
+}
+
+/** Hightlights *********************************/ 
+span.highlight.one {background:#D40000;}
+/** / Hightlights *********************************/ 
+
+/** Shortcodes *********************************/ 
+.accordion .ui-icon,.revealbox .ui-icon { background-color:#D40000; }
+.accordion .ui-state-active .ui-icon {background-color:#990000;  }
+/** / Shortcodes *********************************/ 
+
+.stage-control #stage-prev,.stage-control #stage-next,.stage-control #stage-pause,.stage-control #stage-resume, .stage-control .poststage-prev,.stage-control .poststage-next,.stage-control .poststage-pause,.stage-control .poststage-resume  {background-color:#D40000;} 
+.stage-control #stage-prev:hover,.stage-control #stage-next:hover,.stage-control #stage-pause:hover,.stage-control #stage-resume:hover, .stage-control .poststage-prev:hover,.stage-control .poststage-next:hover,.stage-control .poststage-pause:hover,.stage-control .poststage-resume:hover  {background-color:#990000;}  
+
+
